13d7cdd5fa820af8fdfc52f1c62dc45c626c3076
Malcolm Jones <bossjones@theblacktonystark.com>
2022-11-23 17:26:03 -0500

    wip(centos): better version checking for centos

--- home/shell/centos/env.zsh
+++ home/shell/centos/env.zsh
@@ -1,10 +1,17 @@
 ## On centos we need to custom compile A LOT of stuff, so tell the shell to use it all
-_IS_CENTOS=$(\cat /etc/redhat-release | grep -i centos | wc -l)
-if [[ "${_IS_CENTOS}" == "1" ]]
-then
-    export CC=$HOME/.gcc/10.2.0/bin/gcc
-    export CXX=$HOME/.gcc/10.2.0/bin/g++
-    export FC=$HOME/.gcc/10.2.0/bin/gfortran
-    export LD_LIBRARY_PATH=$HOME/.gcc/10.2.0/lib64
-    . "$HOME/.cargo/env"
+
+check_cmd() {
+    command -v "$1" > /dev/null 2>&1
+}
+
+if check_cmd rpm; then
+    _IS_CENTOS=$(\cat /etc/redhat-release | grep -i centos | wc -l)
+    if [[ "${_IS_CENTOS}" == "1" ]]
+    then
+        export CC=$HOME/.gcc/10.2.0/bin/gcc
+        export CXX=$HOME/.gcc/10.2.0/bin/g++
+        export FC=$HOME/.gcc/10.2.0/bin/gfortran
+        export LD_LIBRARY_PATH=$HOME/.gcc/10.2.0/lib64
+        . "$HOME/.cargo/env"
+    fi
 fi
--- home/shell/centos/path.zsh
+++ home/shell/centos/path.zsh
@@ -1,6 +1,12 @@
 ## On centos we need to custom compile A LOT of stuff, so tell the shell to use it all
-_IS_CENTOS=$(\cat /etc/redhat-release | grep -i centos | wc -l)
-if [[ "${_IS_CENTOS}" == "1" ]]
-then
-    path+=($HOME/.gcc/10.2.0/bin)
+check_cmd() {
+    command -v "$1" > /dev/null 2>&1
+}
+
+if check_cmd rpm; then
+    _IS_CENTOS=$(\cat /etc/redhat-release | grep -i centos | wc -l)
+    if [[ "${_IS_CENTOS}" == "1" ]]
+    then
+        path+=($HOME/.gcc/10.2.0/bin)
+    fi
 fi
