9e1fc1bf282df6bf98e7047c629afe0ed682a7de
Malcolm Jones <bossjones@theblacktonystark.com>
2023-07-02 17:34:36 -0400

    feat: macos

--- home/shell/asdf/env.zsh
+++ home/shell/asdf/env.zsh
@@ -2,3 +2,30 @@ if [[ "$OSTYPE" == linux* ]]
 then
     export ASDF_DIR="${HOME}/.asdf"
 fi
+
+# ------------------
+
+OS="`uname`"
+case $OS in
+  'Linux')
+    OS='Linux'
+    ;;
+  'FreeBSD')
+    OS='FreeBSD'
+    ;;
+  'WindowsNT')
+    OS='Windows'
+    ;;
+  'Darwin')
+    OS='Mac'
+    ;;
+  'SunOS')
+    OS='Solaris'
+    ;;
+  *) ;;
+esac
+
+if [ "$OS" = 'Mac' ]
+then
+    export ASDF_DIR="${HOME}/.asdf"
+fi
--- home/shell/chezmoi/completion.zsh
+++ home/shell/chezmoi/completion.zsh
@@ -4,3 +4,28 @@ if [[ "$OSTYPE" == linux* ]]
 then
     fpath+="$HOME/.zsh/completions"
 fi
+
+OS="`uname`"
+case $OS in
+  'Linux')
+    OS='Linux'
+    ;;
+  'FreeBSD')
+    OS='FreeBSD'
+    ;;
+  'WindowsNT')
+    OS='Windows'
+    ;;
+  'Darwin')
+    OS='Mac'
+    ;;
+  'SunOS')
+    OS='Solaris'
+    ;;
+  *) ;;
+esac
+
+if [ "$OS" = 'Mac' ]
+then
+    fpath+="$HOME/.zsh/completions"
+fi
