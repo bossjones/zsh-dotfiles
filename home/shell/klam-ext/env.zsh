37dde3cc903b1c29f3fcadd94c1069e3fc3ea2f3
Malcolm Jones <bossjones@theblacktonystark.com>
2025-06-22 20:45:25 -0400

    chg: Update CentOS installation scripts to refine package management and improve environment setup
    - Removed the installation of the `libxmlsec1-devel` package from `run_onchange_before_01-centos-install-packages.sh.tmpl`, streamlining the package list for a more efficient setup.
    - Adjusted the `run_before-00-prereq-centos-pyenv.sh.tmpl` script to ensure proper formatting by adding a newline at the end of the file, enhancing script readability and adherence to best practices.
    - These changes contribute to a cleaner and more efficient installation process, improving the overall user experience when setting up CentOS environments.

--- home/shell/klam-ext/env.zsh
+++ home/shell/klam-ext/env.zsh
@@ -1,7 +1,14 @@
-KLAMEXT_BIN="$(which klam-ext)"
+# NOTE: I modified this after seeing how he treats certain conditionals
+# https://github.com/mcornella/dotfiles/blob/main/zshenv
+OS_NAME="$(/usr/bin/uname -s)"
+UNAME_MACHINE="$(/usr/bin/uname -m)"
+if [ "$OS_NAME" = "Darwin" ]; then
 
-if [ -x "$KLAMEXT_BIN" ]; then
-    if command -v klam-ext &>/dev/null; then
-        eval "$(env klam-ext zsh-integration)"
+    KLAMEXT_BIN="$(which klam-ext)"
+
+    if [ -x "$KLAMEXT_BIN" ]; then
+        if command -v klam-ext &>/dev/null; then
+            eval "$(env klam-ext zsh-integration)"
+        fi
     fi
 fi
